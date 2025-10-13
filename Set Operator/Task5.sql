1a3b1ae5f4f3003271b522bc1f2bec3e4bda1b07
Sushant <sushantshirsekar60@gmail.com>
2025-10-14 00:40:47 +0530

    TASK5

---
+++ Set Operator/Task5.sql
@@ -0,0 +1,37 @@
+/* 
+Combine orders and orders archive without having duplicates
+*/
+
+
+SELECT 
+       'Orders' AS SourceTable
+      ,[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.Orders
+UNION
+SELECT 
+       'OrdersArchive' AS SourceTable
+      ,[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.OrdersArchive
+ORDER BY OrderID
