a4b9ccb89a88a66b6446a08d9c46f9dfd46d7223
Sushant <sushantshirsekar60@gmail.com>
2025-10-16 16:09:06 +0530

    Round Function

---
+++ SQL Functions/Numeric Functions/RoundFunc.sql
@@ -0,0 +1,9 @@
+-- Numeric Functions
+-- Round -> Rounds up the decimal
+-- to the value specified
+
+SELECT 
+	3.516,
+ROUND(3.516, 2) AS round_2,
+ROUND(3.516, 1) AS round_1, 
+ROUND(3.516, 0) AS round_0
