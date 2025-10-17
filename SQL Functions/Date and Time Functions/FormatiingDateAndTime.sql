62f57143b281c40fd01d4e44c2e9ec4cc75819a9
Sushant <sushantshirsekar60@gmail.com>
2025-10-17 11:49:56 +0530

    Date and Time Formatting functions

---
+++ SQL Functions/Date and Time Functions/FormatiingDateAndTime.sql
@@ -0,0 +1,15 @@
+-- Formatting Date and Time
+-- Changes date fomrat according to different countries
+
+SELECT 
+	OrderID,
+	CreationTime,
+	FORMAT(CreationTime, 'MM-dd-yyyy') USA_Format,
+	FORMAT(CreationTime, 'dd-MM-yyyy') UK_Format,
+	FORMAT(CreationTime, 'dd') dd,
+	FORMAT(CreationTime, 'ddd')ddd,
+	FORMAT(CreationTime, 'dddd')dddd,
+	FORMAT(CreationTime, 'MM') MM,
+	FORMAT(CreationTime, 'MMM')MMM,
+	FORMAT(CreationTime, 'MMMM')MMMM
+FROM Sales.Orders
