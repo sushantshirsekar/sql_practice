23a1d1d13e7da8cb3af6243a0b206eaa0f553bf2
Sushant <sushantshirsekar60@gmail.com>
2025-10-21 13:30:57 +0530

    Adding Window Function Explanations

--- SQL Functions/Aggregate Functions/Task20.sql
+++ SQL Functions/Aggregate Functions/Task20.sql
@@ -1,5 +1,12 @@
 -- Task 20
 -- Rank the customers based on their total sales
+-- Sequence when using Window Functions
+-- Always add Group By First and then use Window Function
+-- Window Function for advance data analysis
+-- Group By Function for simple data analysis
+-- Window function + Window Definition using Over Clause -> 
+-- Partition By (to aggregate), Order (to implement sequence),
+-- Frame Clause (to define subsets of data)
 
 SELECT 
 	CustomerID,
