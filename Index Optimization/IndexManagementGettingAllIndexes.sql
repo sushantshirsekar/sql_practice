52b7c5d70e35022878db53f0dcea7837610e170b
Sushant <sushantshirsekar60@gmail.com>
2025-10-29 16:59:57 +0530

    Index Management Getting All Indexes

---
+++ Index Optimization/IndexManagementGettingAllIndexes.sql
@@ -0,0 +1,31 @@
+-- ================
+-- Index Management
+-- ================
+
+-- Getting all the indexes for a specific table
+sp_helpindex 'Sales.Customers'
+
+SELECT 
+	tbl.name AS TableName,
+	idx.name AS IndexName,
+	idx.type_desc as IndexType, 
+	idx.is_primary_key as IsPrimaryKey,
+	idx.is_unique as IsUnique,
+	idx.is_disabled as IsDisabled,
+	s.user_seeks AS UserSeeks, 
+	s.user_scans AS UserScans, 
+	s.user_lookups AS UserLookups, 
+	s.user_updates AS UserUpdates,
+	COALESCE(s.last_user_seek, s.last_user_scan)
+FROM sys.indexes idx
+JOIN sys.tables tbl
+ON tbl.object_id = idx.object_id
+LEFT JOIN sys.dm_db_index_usage_stats s
+ON s.index_id = idx.index_id AND
+s.object_id = tbl.object_id
+ORDER BY tbl.name,idx.name
+
+
+SELECT * FROM sys.dm_db_index_usage_stats
+
+SELECT * FROM Sales.Employees
