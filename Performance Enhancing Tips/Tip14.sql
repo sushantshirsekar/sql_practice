b0093106fa2bdc3477ce638ca96761e6875194ab
Sushant <sushantshirsekar60@gmail.com>
2025-11-02 21:35:30 +0530

    Tip 14: Check for Nested Loops and Use SQL HINTS

---
+++ Performance Enhancing Tips/Tip14.sql
@@ -0,0 +1,20 @@
+-- ==================================================
+-- Tipe 14: Check for Nested Loops and Use SQL HINTS
+-- ==================================================
+
+-- Bad Practice (check in execution plan, using nested loops or not)
+SELECT 
+	o.OrderID, 
+	c.FirstName 
+FROM Sales.Customers c
+INNER JOIN Sales.Orders o
+ON c.CustomerID = o.OrderID
+
+-- Good Practice for having Big Table and small table
+SELECT 
+	o.OrderID, 
+	c.FirstName 
+FROM Sales.Customers c
+INNER JOIN Sales.Orders o
+ON c.CustomerID = o.OrderID
+OPTION (HASH JOIN)
